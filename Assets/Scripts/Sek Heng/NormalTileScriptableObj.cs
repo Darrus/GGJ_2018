65e6f4ec3a40fec6d5e9283b190811b1093595ce
Ah Heng <leesekheng@yahoo.com>
2018-01-27 06:19:49 +0800

    Finished A star path finsing with bugs hidden inside

---
+++ Assets/Scripts/Sek Heng/NormalTileScriptableObj.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "MyTiles", menuName = "NormalTileAsset")]
+public class NormalTileScriptableObj : Tile {
+    public string m_WhatName = "LOL";
+}
---
+++ Assets/Scripts/Sek Heng/TileSystem.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/// <summary>
+/// Used for A* search
+/// </summary>
+public class TileSystem : MonoBehaviour {
+    [SerializeField, Tooltip("Tile map to walk through")]
+    protected Tilemap m_TileMap;
+    [SerializeField] protected Grid m_TileGrid;
+
+    [Header("Debugging")]
+    [SerializeField, Tooltip("Array of the TileScripts")]
+    protected TileScript[] m_WalkableTiles;
+    [SerializeField] List<TileScript> m_SearchedPath = new List<TileScript>();
+
+    protected class AStar_Node
+    {
+        public AStar_Node(TileScript _tileScript, float _gCost, float _hCost, AStar_Node _parent)
+        {
+            m_TileScript = _tileScript;
+            g_cost = _gCost;
+            h_cost = _hCost;
+            m_Parent = _parent;
+        }
+
+        public TileScript m_TileScript;
+        public float g_cost; // From start to here.
+        public float h_cost; // From here to the end.
+        public float f_cost
+        {
+            get { return g_cost + h_cost; }
+        }
+        public AStar_Node m_Parent;
+    }
+
+	// Use this for initialization
+	IEnumerator Start () {
+        // delay by a frame so that the tiles will be instantiated
+        yield return null;
+        m_WalkableTiles = m_TileMap.GetComponentsInChildren<TileScript>();
+        if (!m_TileGrid)
+        {
+            m_TileGrid = GetComponent<Grid>();
+        }
+        yield break;
+    }
+
+    List<TileScript> GetNeighbourTiles(TileScript _tile)
+    {
+        List<TileScript> returnList = new List<TileScript>();
+        int cellSizeX = (int)m_TileGrid.cellSize.x;
+        int cellSizeY = (int)m_TileGrid.cellSize.y;
+        int centerPositionOfTileX = (int)_tile.transform.position.x;
+        int centerPositionOfTileY = (int)_tile.transform.position.y;
+        // base on the tile position and grid's tile size!
+        for (int x = -cellSizeX; x <= cellSizeX; x += cellSizeX)
+        {
+            for (int y = -cellSizeY; y <= cellSizeY; y += cellSizeY)
+            {
+                // making sure centers and corners arent allowed
+                if (x == 0 && y == 0 || (Mathf.Abs(x) == 1 && Mathf.Abs(y) == 1))
+                    continue;
+                GameObject neighbourTileGO = m_TileMap.GetInstantiatedObject(new Vector3Int(x + centerPositionOfTileX, y + centerPositionOfTileY, 0));
+                if (neighbourTileGO)
+                {
+                    returnList.Add(neighbourTileGO.GetComponent<TileScript>());
+                }
+            }
+        }
+        return returnList;
+    }
+
+    public List<TileScript> GetFoundPath(TileScript _startTile, TileScript _endTile)
+    {
+        m_SearchedPath.Clear();
+        AStar_Node endNodePath = null;
+        List<AStar_Node> openSet = new List<AStar_Node>();
+        List<TileScript> openSetTileScript = new List<TileScript>();
+        HashSet<TileScript> closedSet = new HashSet<TileScript>();
+        AStar_Node startPathNode = new AStar_Node(_startTile, 0, Vector3.Distance(_startTile.transform.position, _endTile.transform.position), null);
+        openSet.Add(startPathNode);
+        openSetTileScript.Add(_startTile);
+        while (openSet.Count > 0)
+        {
+            // get cheapest path
+            AStar_Node cheapestNode = null;
+            foreach (AStar_Node node in openSet)
+            {
+                if (cheapestNode == null || cheapestNode.f_cost > node.f_cost)
+                {
+                    cheapestNode = node;
+                }
+            }
+            if (cheapestNode.m_TileScript == _endTile)
+            {
+                endNodePath = cheapestNode;
+                break;
+            }
+
+            // then begin the searching of the neighbours!
+            List<TileScript> neighbourOfCheapNode = GetNeighbourTiles(cheapestNode.m_TileScript);
+            foreach (TileScript neighbourTile in neighbourOfCheapNode)
+            {
+                // make sure it is not in the open set and it is walkable
+                if (neighbourTile.TileType == TileScript.TILE_TYPE.WALKABLE && !openSetTileScript.Contains(neighbourTile) && !closedSet.Contains(neighbourTile))
+                {
+                    float GCost = Vector3.Distance(_startTile.transform.position, neighbourTile.transform.position) + cheapestNode.g_cost;
+                    float HCost = Vector3.Distance(neighbourTile.transform.position, _endTile.transform.position);
+                    AStar_Node nodePath = new AStar_Node(neighbourTile, GCost, HCost, cheapestNode);
+                    // then we can add it in the open list
+                    openSetTileScript.Add(neighbourTile);
+                    openSet.Add(nodePath);
+                }
+            }
+            closedSet.Add(cheapestNode.m_TileScript);
+            openSet.Remove(cheapestNode);
+            openSetTileScript.Remove(cheapestNode.m_TileScript);
+        }
+        // then we can start retracing the path
+        if (endNodePath != null)
+        {
+            AStar_Node startTracingNode = endNodePath;
+            while (startTracingNode != null)
+            {
+                m_SearchedPath.Add(startTracingNode.m_TileScript);
+                startTracingNode = startTracingNode.m_Parent;
+            }
+            // reverse since it is from end to start!
+            m_SearchedPath.Reverse();
+        }
+        return m_SearchedPath;
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        foreach (TileScript TileDataStuff in m_WalkableTiles)
+        {
+            if (m_SearchedPath.Contains(TileDataStuff))
+            {
+                Gizmos.color = Color.blue;
+            }
+            else
+            {
+                switch (TileDataStuff.TileType)
+                {
+                    case TileScript.TILE_TYPE.INACCESSIBLE:
+                        Gizmos.color = Color.red;
+                        break;
+                    default:
+                        // then it should be normally walkable
+                        Gizmos.color = Color.white;
+                        break;
+                }
+            }
+            Gizmos.DrawCube(TileDataStuff.transform.position, TileDataStuff.transform.localScale - new Vector3(0.1f,0.1f,0.1f));
+        }
+    }
+#endif
+}
