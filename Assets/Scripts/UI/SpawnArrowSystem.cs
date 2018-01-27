bd11500e2105309c9562da5ad4fabcf1d8506e09
Ah Heng <leesekheng@yahoo.com>
2018-01-27 21:45:25 +0800

    Now the canvas will spawn the arrow if the enemy unit

---
+++ Assets/Scripts/UI/SpawnArrowSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnArrowSystem : MonoBehaviour {
+    [Header("Variables required")]
+    [SerializeField]
+    protected GameObject m_ArrowUnitPrefab;
+
+    private void OnEnable()
+    {
+        SubscriptionSystem.Instance.SubscribeEvent<Transform>("SpawnArrow", SpawnUnitArrow);
+    }
+
+    private void OnDisable()
+    {
+        SubscriptionSystem.Instance.UnsubscribeEvent<Transform>("SpawnArrow", SpawnUnitArrow);
+    }
+
+    protected void SpawnUnitArrow(Transform _trackedUnitTransform)
+    {
+        ArrowPointToUnit arrowPointer = Instantiate(m_ArrowUnitPrefab, transform, false).GetComponent<ArrowPointToUnit>();
+        arrowPointer.TargetTransform = _trackedUnitTransform;
+    }
+}
